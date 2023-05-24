34748792a35182e89faa204790a300f48bad9234
Anderson <112644138+iskowan@users.noreply.github.com>
2023-05-24 09:01:26 -0400

    Commit 5

--- VideoGameQuiz/QuestionsViewController.swift
+++ VideoGameQuiz/QuestionsViewController.swift
@@ -19,7 +19,18 @@ class QuestionsViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     @IBOutlet var button4: UIButton!
     
+    var questionIndex = 0
+    var imageNumber = 1
     
+    func nextQuestion() {
+        questionIndex += 1
+        imageNumber += 1
+        if questionIndex < questions.count {
+            updateUI()
+        } else {
+          performSegue(withIdentifier: "Results", sender: nil)
+        }
+    }
     
     
     var questions: [Question] = [
@@ -60,18 +71,52 @@ class QuestionsViewController: UIViewController {
             Answer(text: "Yunobo", type: .wrong)
         ]),
     ]
+    
+    var answersChosen: [Answer] = []
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
 
-    func newQuestion() {
-        
+            switch sender {
+            case button1:
+                answersChosen.append(currentAnswers[0])
+            case button2:
+                answersChosen.append(currentAnswers[1])
+            case button3:
+                answersChosen.append(currentAnswers[2])
+            case button4:
+                answersChosen.append(currentAnswers[3])
+            default:
+                break
+            }
+        nextQuestion()
+    }
+    
+    func newQuestion(using answers: [Answer]) {
+        button1.setTitle(answers[0].text, for: .normal)
+        button2.setTitle(answers[1].text, for: .normal)
+        button3.setTitle(answers[2].text, for: .normal)
+        button4.setTitle(answers[3].text, for: .normal)
+        questionImageView.image = UIImage(named: "\(imageNumber)")
     }
         
-        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
         // Do any additional setup after loading the view.
     }
     
+    func updateUI() {
+        let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        navigationItem.title = "Question #\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        newQuestion(using: currentAnswers)
+        
+    }
     
     /*
     // MARK: - Navigation
--- VideoGameQuiz/ResultsViewController.swift
+++ VideoGameQuiz/ResultsViewController.swift
@@ -8,29 +8,32 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    
     @IBOutlet var resultsLavel: UILabel!
     @IBOutlet var correctionBar: UIProgressView!
     @IBOutlet var descriptionLabel: UILabel!
     
     @IBOutlet var restartButton: UIButton!
     
+    var responses: [Answer] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
+    
+    // Do any additional setup after loading the view.
+    
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
 }
